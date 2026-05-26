25cf7319836d9d0dd6f8038cddf44bf9d9cc5ff4
ssx <ssxmojc@163.com>
2026-05-26 16:55:17 +0800

    feat: vless add user control

--- protocol/vless/inbound.go
+++ protocol/vless/inbound.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net"
 	"os"
+	"sync"
 
 	"github.com/sagernet/sing-box/adapter"
 	"github.com/sagernet/sing-box/adapter/inbound"
@@ -43,15 +44,17 @@ type Inbound struct {
 	service   *vless.Service[int]
 	tlsConfig tls.ServerConfig
 	transport adapter.V2RayServerTransport
+	userCons  sync.Map
 }
 
 func NewInbound(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.VLESSInboundOptions) (adapter.Inbound, error) {
 	inbound := &Inbound{
-		Adapter: inbound.NewAdapter(C.TypeVLESS, tag),
-		ctx:     ctx,
-		router:  uot.NewRouter(router, logger),
-		logger:  logger,
-		users:   options.Users,
+		Adapter:  inbound.NewAdapter(C.TypeVLESS, tag),
+		ctx:      ctx,
+		router:   uot.NewRouter(router, logger),
+		logger:   logger,
+		users:    options.Users,
+		userCons: sync.Map{},
 	}
 	var err error
 	inbound.router, err = mux.NewRouterWithOptions(inbound.router, logger, common.PtrValueOrDefault(options.Multiplex))
@@ -162,6 +165,10 @@ func (h *Inbound) NewConnectionEx(ctx context.Context, conn net.Conn, metadata a
 		N.CloseOnHandshakeFailure(conn, onClose, err)
 		h.logger.ErrorContext(ctx, E.Cause(err, "process connection from ", metadata.Source))
 	}
+	h.userCons.Store(conn, metadata.User)
+	onClose = N.AppendClose(onClose, func(err error) {
+		h.userCons.Delete(conn)
+	})
 }
 
 func (h *Inbound) newConnectionEx(ctx context.Context, conn net.Conn, metadata adapter.InboundContext, onClose N.CloseHandlerFunc) {
